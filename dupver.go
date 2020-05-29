196207c39c58d0e7d1ecf4c5d4bc457ee4cc147c
Art Barnes <art@pin3.io>
2020-05-29 08:31:57 -0600

    add zip reader

--- dupver.go
+++ dupver.go
@@ -19,6 +19,61 @@ func check(e error) {
     }
 }
 
+func Unzip(src string, dest string) ([]string, error) {
+
+    var filenames []string
+
+    r, err := zip.OpenReader(src)
+    if err != nil {
+        return filenames, err
+    }
+    defer r.Close()
+
+    for _, f := range r.File {
+
+        // Store filename/path for returning and using later on
+        fpath := filepath.Join(dest, f.Name)
+
+        // Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+        if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+            return filenames, fmt.Errorf("%s: illegal file path", fpath)
+        }
+
+        filenames = append(filenames, fpath)
+
+        if f.FileInfo().IsDir() {
+            // Make Folder
+            os.MkdirAll(fpath, os.ModePerm)
+            continue
+        }
+
+        // Make File
+        if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+            return filenames, err
+        }
+
+        outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+        if err != nil {
+            return filenames, err
+        }
+
+        rc, err := f.Open()
+        if err != nil {
+            return filenames, err
+        }
+
+        _, err = io.Copy(outFile, rc)
+
+        // Close the file without defer to close before next iteration of loop
+        outFile.Close()
+        rc.Close()
+
+        if err != nil {
+            return filenames, err
+        }
+    }
+    return filenames, nil
+}
 
 func main() {
 	filePtr := flag.String("file", "ACTIVSg70k.RAW", "an int")
@@ -34,6 +89,61 @@ func main() {
 
 	if (*backupPtr == true) {
 		fmt.Println("Backing up ", filePath)
+
+
+		var filenames []string
+
+		r, err := zip.OpenReader(filePath)
+		if err != nil {
+			return filenames, err
+		}
+		defer r.Close()
+	
+		for _, f := range r.File {
+	
+			// Store filename/path for returning and using later on
+			fpath := filepath.Join(dest, f.Name)
+	
+			// Check for ZipSlip. More Info: http://bit.ly/2MsjAWE
+			if !strings.HasPrefix(fpath, filepath.Clean(dest)+string(os.PathSeparator)) {
+				return filenames, fmt.Errorf("%s: illegal file path", fpath)
+			}
+	
+			filenames = append(filenames, fpath)
+	
+			if f.FileInfo().IsDir() {
+				// Make Folder
+				os.MkdirAll(fpath, os.ModePerm)
+				continue
+			}
+	
+			// Make File
+			if err = os.MkdirAll(filepath.Dir(fpath), os.ModePerm); err != nil {
+				return filenames, err
+			}
+	
+			outFile, err := os.OpenFile(fpath, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, f.Mode())
+			if err != nil {
+				return filenames, err
+			}
+	
+			rc, err := f.Open()
+			if err != nil {
+				return filenames, err
+			}
+	
+			_, err = io.Copy(outFile, rc)
+	
+			// Close the file without defer to close before next iteration of loop
+			outFile.Close()
+			rc.Close()
+	
+			if err != nil {
+				return filenames, err
+			}
+		}
+		return filenames, nil
+	
 
 		// chunky =  Chunker(rd io.Reader, pol Pol) 
 		f, _ := os.Open(filePath)
