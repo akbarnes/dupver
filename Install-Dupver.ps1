7c8d7473c15b0bc3f0a0a5929779c4ae76f02a9e
Art Barnes <art@pin3.io>
2020-06-13 22:46:49 -0600

    move repo init code to func

--- Install-Dupver.ps1
+++ Install-Dupver.ps1
@@ -1,6 +1,6 @@
 go get github.com/restic/chunker
 go get github.com/BurntSushi/toml
-go build dupver.go commit.go pack.go config.go randstring.go util.go
+go build dupver.go repo.go snapshot.go pack.go config.go randstring.go util.go
 echo "Done building"
 
 $InstallFolder = "$HOME\AppData\Local\Executables"
@@ -8,6 +8,6 @@ if (-not (test-path $InstallFolder)) { mkdir $Installfolder }
 if (test-path $InstallFolder\dupver.exe) { del $InstallFolder\dupver.exe }
 move dupver.exe $InstallFolder
 
-$ParentDir = "$HOME\Data\Static\Electric\Transmission Atlas"
+$ParentDir = "$HOME\OneDrive\Documents"
 copy Test-Dupver.ps1 $ParentDir
 echo "Copied to $ParentDir"
--- dupver.go
+++ dupver.go
@@ -67,25 +67,7 @@ func main() {
 	
 
 	if initRepoFlag {
-        fmt.Printf("Creating folder %s\n", repoPath)
-		os.Mkdir(repoPath, 0777)
-
-		packPath := path.Join(repoPath, "packs")
-        fmt.Printf("Creating folder %s\n", packPath)
-		os.Mkdir(packPath, 0777)
-
-	    treesPath := path.Join(repoPath, "trees")
-        fmt.Printf("Creating folder %s\n", treesPath)
-		os.Mkdir(treesPath, 0777)		
-
-	    snapshotsPath := path.Join(repoPath, "snapshots")
-        fmt.Printf("Creating folder %s\n", snapshotsPath)
-		os.Mkdir(snapshotsPath, 0777)
-
-		var myConfig repoConfig
-		myConfig.Version = 1
-		myConfig.ChunkerPolynomial = 0x3DA3358B4DC173
-		SaveRepoConfig(repoPath, myConfig)
+		InitRepo(workDir)
 	} else if initWorkDirFlag {
 		var configPath string
 
