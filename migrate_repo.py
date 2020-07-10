0d35d1287577a5fad67deeb36bea420def5c50f8
Art Barnes <artbarnes@ieee.org>
2020-07-10 12:11:24 -0600

    finish python migration script

--- migrate_repo.py
+++ migrate_repo.py
@@ -1,13 +1,96 @@
-import json, glob, argparse, os
+import json, argparse, os, os.path, toml
+from glob import glob
 
-parser = argparse.ArgumentParser()
-parser.add_argument('workdir')
-args = parser.parse_args()
+pwd = os.getcwd()
+_, workdir = os.path.split(pwd)
 
-workdir = args.workdir
+print(f"Workdir: {pwd} -> {workdir}")
 
-if workdir is None:
-    pwd = os.getcwd()
-    _, workdir = os.path.split(pwd)
+with open(".dupver/config.toml") as f:
+    cfg = toml.load(f)
 
-print(workdir)
+## Update repos to v0.4 format
+# 1. Add prev pointers to snapshot files
+# 2. Add branch & head pointers
+# 3. Convert snapshot filenames to drop date
+
+# glob the snapshots
+snapshot_path = os.path.join(cfg['RepoPath'], 'snapshots', cfg['WorkDirName'])
+print(f'Snapshot path: {snapshot_path}\n')
+snapshots = glob(os.path.join(snapshot_path, '*-*.json'))
+snapshots = sorted(snapshots)
+
+if len(snapshots) == 0:
+    raise ValueError("No snapshots found")
+
+
+print(f'Snapshot:\n{snapshots[0]}')
+
+with open(snapshots[0]) as f:
+    s = json.load(f)
+
+cid = s['ID']
+
+if 'ParentIDs' in s:
+    print(f"Non-empty parent id {s['ParentIDs']} in first file")
+else:
+    print('No parent ids')
+    s['ParentIDs'] = []
+    s.pop('Tags')
+    new_snapshot_path = os.path.join(cfg['RepoPath'], 'snapshots', cfg['WorkDirName'], f"{cid}.json")
+    print(f"Writing to {new_snapshot_path}")
+
+    with open(new_snapshot_path, 'w') as f:
+        json.dump(s, f, indent=2)
+
+pid = cid
+
+for i in range(1,len(snapshots)):
+    sp = snapshots[i]
+
+    print(f'\nSnapshot:\n{sp}')
+
+    with open(sp) as f:
+        s = json.load(f)
+
+    cid = s['ID']
+
+    if 'ParentIDs' in s:
+        if len(s['ParentIDs'] == 2) and len(s['ParentIds'][1]) == 0:
+            print(f"Removing empty id from {s['ParentIDs']}")
+            s['ParentIDs'] = [s['ParentIDs'][0]]
+        else:            
+            print(f"Non-empty parent id {s['ParentIDs']}, skipping")
+            pid = cid
+            continue
+    else:
+        print('No parent ids')
+        s['ParentIDs'] = [pid]
+
+    # save as cid
+    s.pop('Tags')
+    new_snapshot_path = os.path.join(cfg['RepoPath'], 'snapshots', cfg['WorkDirName'], f"{cid}.json")
+    print(f"Writing to {new_snapshot_path}")
+
+    with open(new_snapshot_path, 'w') as f:
+        json.dump(s, f, indent=2)
+
+    pid = cid
+
+    # now create head, branch
+    h = {'CommitID': cid, 'BranchName': 'main'}
+    b = {'CommitID': cid}
+
+    with open('.dupver/head.toml','w') as f:
+        toml.dump(h, f)
+
+    branch_folder = os.path.join(cfg['RepoPath'], 'branches', cfg['WorkDirName'])
+    branch_path = os.path.join(branch_folder, "main.toml")
+
+    if not os.path.exists(branch_folder):
+        os.makedirs(branch_folder)    
+
+    print(f"Writing to {branch_path}")
+    with open(branch_path, 'w') as f:
+        toml.dump(b, f)
+
--- rename-snapshots.fish
+++
@@ -1,9 +0,0 @@
-#!/usr/local/bin/fish
-
-for f in *.json
-    set f2 (echo $f | cut -c 22-67)
-    echo "cp -p $f $f2"
-    echo "rm $f"
-    cp -p "$f" "$f2"
-    # rm "$f"
-end
