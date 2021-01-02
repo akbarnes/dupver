9138c34178af10d4d0c586663549b6415a9dfc32
Art Barnes <art@pin3.io>
2021-01-02 10:59:49 -0700

    copy whole project

--- src/cmd/copy.go
+++ src/cmd/copy.go
@@ -3,10 +3,12 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/akbarnes/dupver/src/dupver"
 	"github.com/spf13/cobra"
 )
 
 var UseDestPath bool
+var CopyAllProjects bool
 
 // snapshots:
 //	 project1:
@@ -35,16 +37,77 @@ var UseDestPath bool
 
 // copyCmd represents the copy command
 var copyCmd = &cobra.Command{
-	Use:   "copy",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:   "copy <dest> [snapshot_id]",
+	Short: "Copy snapshots and chunks to the specified repo",
+	Long: `This command will copy both snapshots and chunks from
+either the default repo or a specified repo to the destination repo.
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+An alternate source repo can be specified with the --repo-name and
+--repo-path flags. The destination repo is specified with the 
+required first positional argument. By default, the destination
+repo is specified from one of the repos current in the working 
+directory configuration file. However, if it desired to specify
+an arbitrary path that can be done by using the --path Boolean
+flag, which instructs dupver to interpret the destination 
+argument as a path. A second optional positional argument 
+will limit only a single specified snapshot id to be copied.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("copy called")
+		cfg := dupver.ReadWorkDirConfig(WorkDirPath)
+		opts := dupver.SetVerbosity(dupver.Options{Color: true}, Debug, Verbose, Quiet)
+
+		if len(RepoName) == 0 {
+			RepoName = cfg.DefaultRepo
+		}
+
+		if len(RepoPath) == 0 {
+			RepoPath = cfg.Repos[RepoName]
+
+			if opts.Verbosity >= 2 {
+				fmt.Printf("Updating repo path to %s\n", RepoPath)
+			}
+		}
+
+		opts.WorkDirName = cfg.WorkDirName
+		opts.RepoName = RepoName
+		opts.RepoPath = RepoPath
+
+		sourcePath := opts.RepoPath
+
+		if len(RepoPath) > 0 {
+			sourcePath = RepoPath
+		}
+
+		destPath := args[0]
+
+		if !UseDestPath {
+			destPath = cfg.Repos[args[0]]
+		}
+
+		if Monochrome || Quiet {
+			opts.Color = false
+		}
+
+		snapshotId := ""
+		// TODO: look up the snapshot id based on 1st n characters
+
+		if len(args) >= 2 {
+			snapshotId = dupver.GetFullSnapshotId(args[1], opts)
+			dupver.CopySnapshot(snapshotId, sourcePath, destPath, opts)
+		} else {
+			//dupver.CopyRepo(opts)
+			snapshotPaths := dupver.ListSnapshots(opts)
+			// print a specific revision
+			for _, snapshotPath := range snapshotPaths {
+				snap := dupver.ReadSnapshotFile(snapshotPath)
+
+				// fmt.Printf("Path: %s\n", snapshotPath)
+				if opts.Verbosity >= 2 {
+					fmt.Printf("Copying snapshot %s\n", snap.ID)
+				}
+
+				dupver.CopySnapshot(snap.ID, sourcePath, destPath, opts)
+			}
+		}
 	},
 }
 
@@ -57,6 +120,7 @@ func init() {
 	// and all subcommands, e.g.:
 	// copyCmd.PersistentFlags().String("foo", "", "A help for foo")
 	copyCmd.PersistentFlags().BoolVarP(&UseDestPath, "path", "p", false, "specify destination repo path instead of name")
+	copyCmd.PersistentFlags().BoolVarP(&CopyAllProjects, "all-projects", "a", false, "copy all projects")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
