f673e01bc8a1729ae38182f6ea06a9cf58885b71
Art Barnes <art@pin2.io>
2022-01-25 20:25:26 -0700

    update fish script

--- preprocess-archives.fish
+++ preprocess-archives.fish
@@ -1,9 +1,10 @@
 #!/usr/bin/fish
 
 set arch_types (cat .dupver_archive_types)
+mkdir -p .dupver_archives/extracted
+set file_list .dupver_archives/archive_files.txt
 
 for arch_type in $arch_types
-
     if test (string length $arch_type) -eq 0
         continue
     end
@@ -18,9 +19,14 @@ for arch_type in $arch_types
         end
 
         set arch_folder (string replace ".$arch_type" "_$arch_type" $arch_file)
-        set extracted_folder ".dupver_archives/$arch_folder"
+        set extracted_folder ".dupver_archives/extracted/$arch_folder"
         rm -fr "$extracted_folder"
         mkdir -p "$extracted_folder"
         unzip -d "$extracted_folder" $arch_file  
+
+        echo "$arch_file" >> $file_list
+        echo "$extracted_folder" >> $file_list
+        echo '' >> $file_list
     end
 end
+
