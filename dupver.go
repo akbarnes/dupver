271da94226b1d92291eb4ad546bc8571f78be84d
Art Barnes <art@pin2.io>
2022-02-22 18:21:51 -0700

    update versions

--- dupver.go
+++ dupver.go
@@ -1,10 +1,10 @@
 package dupver
 
 const (
-	MajorVersion       = 3
+	MajorVersion       = 4
 	MinorVersion       = 0
 	PatchVersion       = 0
-	RepoMajorVersion   = 3
+	RepoMajorVersion   = 4
 	RepoMinorVersion   = 0
 	PrefsMajorVersion  = 2
 	PrefsMinorVersion  = 0
